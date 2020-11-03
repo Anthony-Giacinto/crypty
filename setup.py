8609cf9f6eee27e75ff1ca23f8792e377b62ebbb
Anthony-Giacinto <anthonygiacinto1@gmail.com>
2020-11-03 15:15:09 -0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(name='crypty',
+      version='1.0',
+      description='Allows the user to encrypt/decrypt morse code and binary.',
+      url='https://github.com/Anthony-Giacinto/crypty',
+      author='Anthony Giacinto',
+      author_email='anthonygiacinto1@gmail.com',
+      packages=['crypty'])
